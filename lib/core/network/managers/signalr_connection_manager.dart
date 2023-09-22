d3b7d7be818cc441861c8e83c8266ea624d7c652
Emre Reyhanlioglu <emre.reyhanlioglu@commencis.com>
2023-09-22 14:48:53 +0300

    Add network layer

---
+++ lib/core/network/managers/signalr_connection_manager.dart
@@ -0,0 +1,76 @@
+/*
+ * burgan_core
+ *
+ * Created on 22/9/2023.
+ * Copyright (c) 2023 Commencis. All rights reserved.
+ *
+ * Save to the extent permitted by law, you may not use, copy, modify,
+ * distribute or create derivative works of this material or any part
+ * of it without the prior written consent of Commencis.
+ * Any reproduction of this material must contain this notice.
+ */
+
+import 'dart:convert';
+
+import 'package:burgan_core/core/network/models/brg_signalr_transition.dart';
+import 'package:collection/collection.dart';
+import 'package:signalr_netcore/signalr_client.dart';
+
+class SignalrConnectionManager {
+  final String serverUrl;
+  final String methodName;
+  late HubConnection _hubConnection;
+
+  SignalrConnectionManager({
+    required this.serverUrl,
+    required this.methodName,
+  });
+
+  Future<void> init({Function(String navigationPath)? onPageNavigation}) async {
+    _hubConnection = HubConnectionBuilder()
+        .withUrl(serverUrl)
+        .withAutomaticReconnect(retryDelays: [2000, 5000, 10000, 20000]).build();
+    _hubConnection.onclose(({error}) => print('onclose called'));
+    _hubConnection.onreconnecting(({error}) => print("onreconnecting called"));
+    _hubConnection.onreconnected(({connectionId}) => print("onreconnected called"));
+
+    if (_hubConnection.state != HubConnectionState.Connected) {
+      await _hubConnection.start();
+    }
+  }
+
+  void listenForTransitionEvents({
+    required String transitionId,
+    required Function(String navigationPath) onPageNavigation,
+    Function(String errorMessage)? onError,
+  }) {
+    _hubConnection.on(methodName, (List<Object?>? transitions) {
+      if (transitions == null) {
+        return;
+      }
+      final ongoingTransition = transitions
+          .map((transition) {
+            try {
+              return BrgSignalRTransition.fromJson(jsonDecode(transition as String));
+            } catch (e) {
+              return null;
+            }
+          })
+          .where((element) => element != null)
+          .toList()
+          .firstWhereOrNull((transition) => transition?.transitionId == transitionId);
+
+      final isNavigationAllowed = ongoingTransition?.pageDetails["operation"] == "Open";
+      final navigationPath = ongoingTransition?.pageDetails["pageRoute"]?["label"] as String?;
+      if (isNavigationAllowed && navigationPath != null) {
+        onPageNavigation(navigationPath);
+      } else if ((ongoingTransition?.errorMessage.isNotEmpty ?? false) && onError != null) {
+        onError(ongoingTransition!.errorMessage);
+      }
+    });
+  }
+
+  void stop() {
+    _hubConnection.stop();
+  }
+}
---
+++ lib/core/network/models/brg_signalr_transition.dart
@@ -0,0 +1,39 @@
+/*
+ * burgan_core
+ *
+ * Created on 22/9/2023.
+ * Copyright (c) 2023 Commencis. All rights reserved.
+ *
+ * Save to the extent permitted by law, you may not use, copy, modify,
+ * distribute or create derivative works of this material or any part
+ * of it without the prior written consent of Commencis.
+ * Any reproduction of this material must contain this notice.
+ */
+
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'brg_signalr_transition.g.dart';
+
+@JsonSerializable(createToJson: false)
+class BrgSignalRTransition extends Equatable {
+  @JsonKey(name: "transition")
+  final String transitionId;
+
+  @JsonKey(name: "page", defaultValue: {})
+  final Map<String, dynamic> pageDetails;
+
+  @JsonKey(name: "message", defaultValue: "")
+  final String errorMessage;
+
+  const BrgSignalRTransition({
+    required this.transitionId,
+    required this.pageDetails,
+    required this.errorMessage,
+  });
+
+  @override
+  List<Object?> get props => [transitionId, pageDetails, errorMessage];
+
+  factory BrgSignalRTransition.fromJson(Map<String, dynamic> json) => _$BrgSignalRTransitionFromJson(json);
+}
