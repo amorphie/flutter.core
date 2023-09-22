24de64c2309f9f76d38a3d605fe99f44bcb114a4
Emre Reyhanlioglu <emre.reyhanlioglu@commencis.com>
2023-09-22 11:53:34 +0300

    Add copyright profile

--- lib/burgan_core.dart
+++ lib/burgan_core.dart
@@ -1,2 +1,14 @@
+/*
+ * burgan_core
+ *
+ * Created on 22/9/2023.
+ * Copyright (c) 2023 Commencis. All rights reserved.
+ *
+ * Save to the extent permitted by law, you may not use, copy, modify,
+ * distribute or create derivative works of this material or any part
+ * of it without the prior written consent of Commencis.
+ * Any reproduction of this material must contain this notice.
+ */
+
 export 'core/bus/widget_event_bus/brg_widget_event.dart';
 export 'core/bus/widget_event_bus/brg_widget_event_bus.dart';
--- lib/core/bus/widget_event_bus/brg_widget_event.dart
+++ lib/core/bus/widget_event_bus/brg_widget_event.dart
@@ -1,3 +1,15 @@
+/*
+ * burgan_core
+ *
+ * Created on 22/9/2023.
+ * Copyright (c) 2023 Commencis. All rights reserved.
+ *
+ * Save to the extent permitted by law, you may not use, copy, modify,
+ * distribute or create derivative works of this material or any part
+ * of it without the prior written consent of Commencis.
+ * Any reproduction of this material must contain this notice.
+ */
+
 class BrgWidgetEvent {
   final String eventId;
   final Object? data;
