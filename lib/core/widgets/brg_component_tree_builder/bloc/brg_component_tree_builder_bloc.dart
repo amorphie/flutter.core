de2e15ee97a08c379d1b615cc205fd3c0a9eea18
Emre Reyhanlioglu <emre.reyhanlioglu@commencis.com>
2023-10-23 16:38:48 +0300

    Update network layer of component tree builder widget

--- lib/core/widgets/brg_component_tree_builder/bloc/brg_component_tree_builder_bloc.dart
+++ lib/core/widgets/brg_component_tree_builder/bloc/brg_component_tree_builder_bloc.dart
@@ -1,9 +1,11 @@
+import 'package:burgan_core/core/network/models/neo_response.dart';
+import 'package:burgan_core/core/util/extensions/string_extensions.dart';
 import 'package:burgan_core/core/widgets/brg_component_tree_builder/network/i_components_network_manager.dart';
+import 'package:burgan_core/core/widgets/brg_component_tree_builder/network/models/neo_page_components_response.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'brg_component_tree_builder_event.dart';
-
 part 'brg_component_tree_builder_state.dart';
 
 class BrgComponentTreeBuilderBloc extends Bloc<BrgComponentTreeBuilderEvent, BrgComponentTreeBuilderState> {
@@ -12,8 +14,14 @@ class BrgComponentTreeBuilderBloc extends Bloc<BrgComponentTreeBuilderEvent, Brg
   BrgComponentTreeBuilderBloc(this.networkManager) : super(BrgComponentTreeBuilderStateLoading()) {
     on<BrgComponentTreeBuilderEventFetchComponents>((event, emit) async {
       try {
-        var response = await networkManager.fetchPageComponentsByPageId(event.pageId);
-        emit(BrgComponentTreeBuilderStateLoaded(componentsMap: response));
+        NeoResponse<NeoPageComponentsResponse> response = await networkManager.fetchPageComponentsByPageId(
+          event.pageId,
+        );
+        if (response.isSuccess) {
+          emit(BrgComponentTreeBuilderStateLoaded(componentsMap: ((response as NeoSuccess).data).componentsMap));
+        } else {
+          emit(BrgComponentTreeBuilderStateError(errorMessage: (response as NeoError).error.message.orEmpty));
+        }
       } on Exception catch (e) {
         emit(BrgComponentTreeBuilderStateError(errorMessage: e.toString()));
       }
--- lib/core/widgets/brg_component_tree_builder/network/i_components_network_manager.dart
+++ lib/core/widgets/brg_component_tree_builder/network/i_components_network_manager.dart
@@ -1,5 +1,8 @@
+import 'package:burgan_core/core/network/models/neo_response.dart';
+import 'package:burgan_core/core/widgets/brg_component_tree_builder/network/models/neo_page_components_response.dart';
+
 abstract class IComponentsNetworkManager {
   IComponentsNetworkManager();
 
-  Future<Map<String, dynamic>> fetchPageComponentsByPageId(String pageId);
+  Future<NeoResponse<NeoPageComponentsResponse>> fetchPageComponentsByPageId(String pageId);
 }
