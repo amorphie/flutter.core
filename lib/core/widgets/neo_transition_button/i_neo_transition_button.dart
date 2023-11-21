ba26c8ecf5254a7d0c685b412f21bacf774672c3
Emre Reyhanlıoğlu <reyhanliogluemre@gmail.com>
2023-11-21 10:40:51 +0300

    Update NeoTransitionButton to be able to extend from it

---
+++ lib/core/widgets/neo_transition_button/i_neo_transition_button.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:meta/meta.dart';
+import 'package:neo_core/core/navigation/i_neo_navigation_helper.dart';
+import 'package:neo_core/core/navigation/neo_navigation_type.dart';
+import 'package:neo_core/core/util/neo_core_app_constants.dart';
+import 'package:neo_core/core/workflow_form/bloc/workflow_form_bloc.dart';
+import 'package:neo_core/core/workflow_form/neo_workflow_manager.dart';
+import 'package:neo_core/neo_core.dart';
+
+abstract class INeoTransitionButton extends StatelessWidget {
+  const INeoTransitionButton({
+    required this.entity,
+    required this.transitionId,
+    required this.text,
+    Key? key,
+  }) : super(key: key);
+
+  final String entity;
+  final String transitionId;
+  final String text;
+
+  @nonVirtual
+  @override
+  Widget build(BuildContext context) {
+    final appConstants = GetIt.I<NeoCoreAppConstants>();
+    return NeoTransitionListenerWidget(
+      transitionId: transitionId,
+      signalRServerUrl: appConstants.workflowHubUrl,
+      signalRMethodName: appConstants.workflowMethodName,
+      onPageNavigation: (String navigationPath) => _handleNavigation(context, navigationPath),
+      onError: onTransitionError,
+      child: buildCustom(context),
+    );
+  }
+
+  /// Don't forget to call [onStartTransition] to start transition from custom widget design
+  @visibleForOverriding
+  Widget buildCustom(BuildContext context);
+
+  /// Triggered when there is an error from SignalR
+  @visibleForOverriding
+  void onTransitionError(String errorMessage);
+
+  Future onStartTransition(BuildContext context) {
+    return NeoWorkflowManager(GetIt.I<NeoNetworkManager>()).postTransition(
+      entity: entity,
+      transitionId: transitionId,
+      body: _getFormParametersIfExist(context),
+    );
+  }
+
+  Map<String, dynamic> _getFormParametersIfExist(BuildContext context) {
+    Map<String, dynamic> data = {};
+    try {
+      data = context.read<WorkflowFormBloc>().formData;
+    } on Exception catch (_) {
+      // no-op
+    }
+    return data;
+  }
+
+  void _handleNavigation(BuildContext context, String navigationPath) {
+    GetIt.I<INeoNavigationHelper>().navigate(
+      context: context,
+      // STOPSHIP: Get navigation type from signalR
+      navigationType: NeoNavigationType.pushReplacement,
+      navigationPath: navigationPath,
+    );
+  }
+}
--- lib/core/widgets/neo_transition_button/neo_transition_button.dart
+++ lib/core/widgets/neo_transition_button/neo_transition_button.dart
@@ -1,73 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get_it/get_it.dart';
-import 'package:neo_core/core/navigation/i_neo_navigation_helper.dart';
-import 'package:neo_core/core/navigation/neo_navigation_type.dart';
-import 'package:neo_core/core/util/neo_core_app_constants.dart';
-import 'package:neo_core/core/workflow_form/bloc/workflow_form_bloc.dart';
-import 'package:neo_core/core/workflow_form/neo_workflow_manager.dart';
+import 'package:meta/meta.dart';
+import 'package:neo_core/core/widgets/neo_transition_button/i_neo_transition_button.dart';
 import 'package:neo_core/neo_core.dart';
 
 const _buttonColor = Color(0xFF0069AA); // TODO: Move to colors file
 
-class NeoTransitionButton extends StatefulWidget {
+class NeoTransitionButton extends INeoTransitionButton {
   const NeoTransitionButton({
-    required this.entity,
-    required this.transitionId,
-    required this.text,
-    Key? key,
-  }) : super(key: key);
+    required super.entity,
+    required super.transitionId,
+    required super.text,
+    super.key,
+  });
 
-  final String entity;
-  final String transitionId;
-  final String text;
-
+  /// Don't forget to call [onStartTransition] to start transition
   @override
-  State<NeoTransitionButton> createState() => _NeoTransitionButtonState();
-}
-
-class _NeoTransitionButtonState extends State<NeoTransitionButton> {
-  @override
-  Widget build(BuildContext context) {
-    final appConstants = GetIt.I<NeoCoreAppConstants>();
-    return NeoTransitionListenerWidget(
-      transitionId: widget.transitionId,
-      signalRServerUrl: appConstants.workflowHubUrl,
-      signalRMethodName: appConstants.workflowMethodName,
-      onPageNavigation: (String navigationPath) => _handleNavigation(context, navigationPath),
-      child: FilledButton(
-        style: OutlinedButton.styleFrom(
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-          backgroundColor: _buttonColor,
-        ),
-        onPressed: () {
-          NeoWorkflowManager(GetIt.I<NeoNetworkManager>()).postTransition(
-            entity: widget.entity,
-            transitionId: widget.transitionId,
-            body: _getFormParametersIfExist(context),
-          );
-        },
-        child: Text(widget.text).padding(left: 16, right: 16, top: 20, bottom: 20),
+  @visibleForOverriding
+  Widget buildCustom(BuildContext context) {
+    return FilledButton(
+      style: OutlinedButton.styleFrom(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+        backgroundColor: _buttonColor,
       ),
+      onPressed: () => onStartTransition(context),
+      child: Text(text).padding(left: 16, right: 16, top: 20, bottom: 20),
     );
   }
 
-  Map<String, dynamic> _getFormParametersIfExist(BuildContext context) {
-    Map<String, dynamic> data = {};
-    try {
-      data = context.read<WorkflowFormBloc>().formData;
-    } on Exception catch (_) {
-      // no-op
-    }
-    return data;
-  }
-
-  void _handleNavigation(BuildContext context, String navigationPath) {
-    GetIt.I<INeoNavigationHelper>().navigate(
-      context: context,
-      // STOPSHIP: Get navigation type from signalR
-      navigationType: NeoNavigationType.pushReplacement,
-      navigationPath: navigationPath,
-    );
+  @override
+  void onTransitionError(String errorMessage) {
+    // No-op. Override if necessary
   }
 }
