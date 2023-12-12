3ca983a2060161de72693b1621b7b9eb80d490f4
U05434 <eboz@burgan.com.tr>
2023-12-12 09:56:29 +0300

    EnverifySDK: channel was updated

--- android/app/src/main/kotlin/com/amorphie/core/App.kt
+++ android/app/src/main/kotlin/com/amorphie/core/App.kt
@@ -1,5 +1,16 @@
 package com.amorphie.core
 
 import android.app.Application
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class App : Application()
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        GeneratedPluginRegistrant.registerWith(FlutterEngine(applicationContext))
+    }
+}
+
+
+
