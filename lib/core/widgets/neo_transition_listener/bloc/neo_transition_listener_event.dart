9d513c132d66eb93dd33aaece922817c8823023e
Emre Reyhanlıoğlu <reyhanliogluemre@gmail.com>
2024-07-04 10:37:13 +0300

    NeoTransitionListener: Add isTwoFactorAuthenticated flag to onLoggedInSuccessfully callback

--- lib/core/widgets/neo_transition_listener/bloc/neo_transition_listener_event.dart
+++ lib/core/widgets/neo_transition_listener/bloc/neo_transition_listener_event.dart
@@ -21,7 +21,7 @@ class NeoTransitionListenerEventInit extends NeoTransitionListenerEvent {
   final String signalRMethodName;
   final Function(SignalrTransitionData navigationData) onTransitionSuccess;
   final Function(EkycEventData eventData) onEkycEvent;
-  final VoidCallback? onLoggedInSuccessfully;
+  final Function({required bool isTwoFactorAuthenticated})? onLoggedInSuccessfully;
   final Function(NeoError error)? onTransitionError;
   final Function({required bool displayLoading}) onLoadingStatusChanged;
   final bool bypassSignalr;
