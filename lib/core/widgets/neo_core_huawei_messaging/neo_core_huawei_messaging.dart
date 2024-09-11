6330be368ae34ffa7c0f78f005722816c16d62ce
U05434 <eboz@burgan.com.tr>
2024-09-11 13:24:20 +0300

    Huawei: add HuaweiMessaging

---
+++ lib/core/widgets/neo_core_huawei_messaging/neo_core_huawei_messaging.dart
@@ -0,0 +1,177 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:huawei_push/huawei_push.dart'; // Huawei Push Kit
+import 'package:neo_core/core/network/managers/neo_network_manager.dart';
+import 'package:neo_core/core/storage/neo_core_secure_storage.dart';
+import 'package:neo_core/feature/device_registration/usecases/neo_core_register_device_usecase.dart';
+import 'package:universal_io/io.dart';
+
+abstract class _Constant {
+  static const androidNotificationChannelID = "high_importance_channel";
+  static const androidNotificationChannelName = "High Importance Notifications";
+  static const androidNotificationChannelDescription = "This channel is used for important notifications";
+  static const pushNotificationDeeplinkKey = "deeplink";
+}
+
+@pragma('vm:entry-point')
+Future<void> onBackgroundMessage(RemoteMessage message) async {
+  return Future.value();
+}
+
+class NeoCoreHuaweiMessaging extends StatefulWidget {
+  const NeoCoreHuaweiMessaging({
+    required this.child,
+    required this.networkManager,
+    required this.neoCoreSecureStorage,
+    this.androidDefaultIcon,
+    this.onDeeplinkNavigation,
+    super.key,
+  });
+
+  final Widget child;
+  final NeoNetworkManager networkManager;
+  final NeoCoreSecureStorage neoCoreSecureStorage;
+  final String? androidDefaultIcon;
+  final Function(String)? onDeeplinkNavigation;
+
+  @override
+  State<NeoCoreHuaweiMessaging> createState() => _NeoCoreHuaweiMessagingState();
+}
+
+class _NeoCoreHuaweiMessagingState extends State<NeoCoreHuaweiMessaging> {
+  final _androidChannel = const AndroidNotificationChannel(
+    _Constant.androidNotificationChannelID,
+    _Constant.androidNotificationChannelName,
+    description: _Constant.androidNotificationChannelDescription,
+  );
+  final _localNotifications = FlutterLocalNotificationsPlugin();
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.child;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    if (kIsWeb) {
+      return;
+    }
+    _initNotifications();
+    _initPushNotifications();
+    if (Platform.isAndroid) {
+      _initLocalNotifications();
+    }
+  }
+
+  Future<void> _initNotifications() async {
+    await _getHuaweiToken();
+
+    // Listen to the token stream
+    Push.getTokenStream.listen(_onTokenChange);
+
+    // Listen to the message received stream
+    Push.onMessageReceivedStream.listen(_onMessageReceived, onError: _onMessageReceiveError);
+
+    // Listen to notification opened events
+    Push.onNotificationOpenedApp.listen(_onNotificationOpenedApp);
+  }
+
+  void _onTokenChange(String token) {
+    NeoCoreRegisterDeviceUseCase().call(
+      networkManager: widget.networkManager,
+      secureStorage: widget.neoCoreSecureStorage,
+      deviceToken: token,
+    );
+  }
+
+  Future<void> _initPushNotifications() async {
+    // Enable push notifications for Huawei Push Kit
+    await Push.turnOnPush();
+  }
+
+  Future<void> _initLocalNotifications() async {
+    final String androidIcon = widget.androidDefaultIcon ?? "";
+    final android = AndroidInitializationSettings(androidIcon);
+    final settings = InitializationSettings(android: android);
+
+    await _localNotifications.initialize(
+      settings,
+      onDidReceiveNotificationResponse: (NotificationResponse notificationResponse) {
+        final String? payload = notificationResponse.payload;
+        if (payload != null) {
+          // Directly handle the JSON payload from the notification
+          final Map<String, dynamic> messageData = jsonDecode(payload) as Map<String, dynamic>;
+          _handleMessage(messageData);
+        }
+      },
+    );
+
+    await _localNotifications
+        .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(_androidChannel);
+  }
+
+  void _handleMessage(Map<String, dynamic> messageData) {
+    final String? deeplinkPath = messageData[_Constant.pushNotificationDeeplinkKey];
+    if (deeplinkPath != null && deeplinkPath.isNotEmpty) {
+      widget.onDeeplinkNavigation?.call(deeplinkPath);
+    }
+  }
+
+  Future<void> _getHuaweiToken() async {
+    try {
+      await Push.turnOnPush();
+
+      // Request token (token will be sent via Push.getTokenStream)
+      Push.getToken(""); // Request the token with an empty string
+
+      // Listen to the token stream
+      Push.getTokenStream.listen((token) {
+        if (token != null) {
+          debugPrint("Huawei Push token: $token");
+          _onTokenChange(token);
+        }
+      });
+    } catch (e) {
+      debugPrint("Error getting Huawei push token: $e");
+    }
+  }
+
+  void _onMessageReceived(RemoteMessage message) {
+    final notification = message.notification;
+    if (notification == null || !Platform.isAndroid) {
+      return;
+    }
+    _localNotifications.show(
+      notification.hashCode,
+      notification.title,
+      notification.body,
+      NotificationDetails(
+        android: AndroidNotificationDetails(
+          _androidChannel.id,
+          _androidChannel.name,
+          channelDescription: _androidChannel.description,
+          icon: widget.androidDefaultIcon,
+        ),
+      ),
+      payload: jsonEncode(message.data),
+    );
+  }
+
+  void _onNotificationOpenedApp(dynamic initialNotification) {
+    if (initialNotification != null) {
+      // Handle the initialNotification as a Map<String, dynamic>
+      final Map<String, dynamic> messageData = initialNotification as Map<String, dynamic>;
+      _handleMessage(messageData);
+    }
+  }
+
+  void _onMessageReceiveError(Object error) {
+    debugPrint("Error receiving message: $error");
+  }
+}
