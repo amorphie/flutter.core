f270b2a702277cceaaff6031fcbf4ccf3f6d8c71
U05434 <eboz@burgan.com.tr>
2024-09-25 16:31:18 +0300

    Huawei: added SharedPrefs

--- lib/core/storage/neo_core_parameter_key.dart
+++ lib/core/storage/neo_core_parameter_key.dart
@@ -31,5 +31,7 @@ abstract class NeoCoreParameterKey {
   static const secureStorageUserRole = "secureStorage:infrastructure:userRole";
 
   static const sharedPrefsFirstRun = "sharedPrefs:infrastructure:firstRun";
+  static const sharedPrefsIsGoogleServicesAvailable = "sharedPrefs:common:sharedPrefsIsGoogleServicesAvailable";
+  static const sharedPrefsIsHuaweiCompatible = "sharedPrefs:common:sharedPrefsIsHuaweiCompatible";
   static const sharedPrefsLanguageCode = "sharedPrefs:common:languageCode";
 }
--- lib/core/widgets/neo_core_messaging/neo_core_messaging.dart
+++ lib/core/widgets/neo_core_messaging/neo_core_messaging.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get_it/get_it.dart';
 import 'package:neo_core/core/network/managers/neo_network_manager.dart';
+import 'package:neo_core/core/storage/neo_core_parameter_key.dart';
 import 'package:neo_core/core/storage/neo_core_secure_storage.dart';
+import 'package:neo_core/core/storage/neo_shared_prefs.dart';
 import 'package:neo_core/core/widgets/neo_core_firebase_messaging/neo_core_firebase_messaging.dart';
 import 'package:neo_core/core/widgets/neo_core_huawei_messaging/neo_core_huawei_messaging.dart';
 
 class NeoCoreMessaging extends StatelessWidget {
-  final bool isHuaweiCompatible;
   final Widget child;
   final NeoNetworkManager networkManager;
   final NeoCoreSecureStorage neoCoreSecureStorage;
@@ -15,7 +18,6 @@ class NeoCoreMessaging extends StatelessWidget {
   final Function(String)? onDeeplinkNavigation;
 
   const NeoCoreMessaging({
-    required this.isHuaweiCompatible,
     required this.child,
     required this.networkManager,
     required this.neoCoreSecureStorage,
@@ -28,22 +30,27 @@ class NeoCoreMessaging extends StatelessWidget {
   Widget build(BuildContext context) {
     if (kIsWeb) {
       return child;
-    } else if (isHuaweiCompatible) {
-      return NeoCoreHuaweiMessaging(
-        networkManager: networkManager,
-        neoCoreSecureStorage: neoCoreSecureStorage,
-        androidDefaultIcon: androidDefaultIcon,
-        onDeeplinkNavigation: onDeeplinkNavigation,
-        child: child,
-      );
     } else {
-      return NeoCoreFirebaseMessaging(
-        networkManager: networkManager,
-        neoCoreSecureStorage: neoCoreSecureStorage,
-        androidDefaultIcon: androidDefaultIcon,
-        onDeeplinkNavigation: onDeeplinkNavigation,
-        child: child,
-      );
+      final NeoSharedPrefs neoSharedPrefs = GetIt.I.get();
+      final bool isHuaweiCompatible =
+          neoSharedPrefs.read(NeoCoreParameterKey.sharedPrefsIsHuaweiCompatible) as bool? ?? false;
+      if (isHuaweiCompatible) {
+        return NeoCoreHuaweiMessaging(
+          networkManager: networkManager,
+          neoCoreSecureStorage: neoCoreSecureStorage,
+          androidDefaultIcon: androidDefaultIcon,
+          onDeeplinkNavigation: onDeeplinkNavigation,
+          child: child,
+        );
+      } else {
+        return NeoCoreFirebaseMessaging(
+          networkManager: networkManager,
+          neoCoreSecureStorage: neoCoreSecureStorage,
+          androidDefaultIcon: androidDefaultIcon,
+          onDeeplinkNavigation: onDeeplinkNavigation,
+          child: child,
+        );
+      }
     }
   }
 }
