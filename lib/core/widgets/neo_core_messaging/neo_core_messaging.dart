99d9f626cb8715328dcff9b556cdbaffd8aedda0
U05434 <eboz@burgan.com.tr>
2024-09-25 16:45:19 +0300

    Huawei: Added NeoSharedPrefs in NeoCoreMessaging

--- lib/core/widgets/neo_core_messaging/neo_core_messaging.dart
+++ lib/core/widgets/neo_core_messaging/neo_core_messaging.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:get_it/get_it.dart';
 import 'package:neo_core/core/network/managers/neo_network_manager.dart';
 import 'package:neo_core/core/storage/neo_core_parameter_key.dart';
 import 'package:neo_core/core/storage/neo_core_secure_storage.dart';
@@ -12,6 +11,7 @@ import 'package:neo_core/core/widgets/neo_core_huawei_messaging/neo_core_huawei_
 
 class NeoCoreMessaging extends StatelessWidget {
   final Widget child;
+  final NeoSharedPrefs neoSharedPrefs;
   final NeoNetworkManager networkManager;
   final NeoCoreSecureStorage neoCoreSecureStorage;
   final String? androidDefaultIcon;
@@ -19,6 +19,7 @@ class NeoCoreMessaging extends StatelessWidget {
 
   const NeoCoreMessaging({
     required this.child,
+    required this.neoSharedPrefs,
     required this.networkManager,
     required this.neoCoreSecureStorage,
     this.androidDefaultIcon,
@@ -31,7 +32,6 @@ class NeoCoreMessaging extends StatelessWidget {
     if (kIsWeb) {
       return child;
     } else {
-      final NeoSharedPrefs neoSharedPrefs = GetIt.I.get();
       final bool isHuaweiCompatible =
           neoSharedPrefs.read(NeoCoreParameterKey.sharedPrefsIsHuaweiCompatible) as bool? ?? false;
       if (isHuaweiCompatible) {
