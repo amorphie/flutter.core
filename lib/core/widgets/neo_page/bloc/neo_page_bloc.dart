f6a0a1e4c70a4e050ca1c34c871f380719695e33
Ayhan Cavdar <ayhan.cavdar@commencis.com>
2024-10-02 14:28:45 +0300

    SetCustomFieldValidation: Remove copyright message

--- lib/core/widgets/neo_page/bloc/neo_page_bloc.dart
+++ lib/core/widgets/neo_page/bloc/neo_page_bloc.dart
@@ -1,17 +1,3 @@
-/*
- * 
- * flutter.core
- * 
- * Created on 01/10/2024.
- * Copyright (c) 2024 Commencis. All rights reserved.
- * 
- * Save to the extent permitted by law, you may not use, copy, modify,
- * distribute or create derivative works of this material or any part
- * of it without the prior written consent of Commencis.
- * Any reproduction of this material must contain this notice.
- * 
- */
-
 import 'dart:async';
 
 import 'package:collection/collection.dart';
