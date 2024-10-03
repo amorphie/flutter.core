942430fdf33debc341cf9d3d96b5084a10ed8fce
Alperen Soysal <alperen.soysal@commencis.com>
2024-10-03 18:30:59 +0300

    FirebaseRemoteConfig: Add fail log

--- lib/core/storage/neo_remote_config.dart
+++ lib/core/storage/neo_remote_config.dart
@@ -1,14 +1,19 @@
 import 'package:firebase_remote_config/firebase_remote_config.dart';
+import 'package:get_it/get_it.dart';
+import 'package:neo_core/core/analytics/neo_logger.dart';
 
 abstract class _Constants {
   static const Duration fetchTimeout = Duration(minutes: 1);
   static const Duration minimumFetchInterval = Duration(minutes: 1);
+  static const String initializationFailMessage = "[NeoRemoteConfig]: FirebaseRemoteConfig fetchAndActivate failed!";
 }
 
 class NeoRemoteConfig {
   NeoRemoteConfig();
 
   final FirebaseRemoteConfig _firebaseRemoteConfig = FirebaseRemoteConfig.instance;
+
+  NeoLogger get _neoLogger => GetIt.I.get();
 
   Future<void> init() async {
     await _firebaseRemoteConfig.setConfigSettings(
@@ -17,7 +22,11 @@ class NeoRemoteConfig {
         minimumFetchInterval: _Constants.minimumFetchInterval,
       ),
     );
-    await _firebaseRemoteConfig.fetchAndActivate();
+
+    final bool isSuccess = await _firebaseRemoteConfig.fetchAndActivate();
+    if (isSuccess) {
+      _neoLogger.logError(_Constants.initializationFailMessage);
+    }
   }
 
   Future<void> setDefaults({required Map<String, dynamic> defaultValues}) async {
