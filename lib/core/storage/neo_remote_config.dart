3e4345d531336fab555a94ef2c174e308e9c35b9
Alperen Soysal <alperen.soysal@commencis.com>
2024-10-04 12:03:52 +0300

    FirebaseRemoteConfig: Add exception handling & fix interval durations

--- lib/core/storage/neo_remote_config.dart
+++ lib/core/storage/neo_remote_config.dart
@@ -3,9 +3,10 @@ import 'package:get_it/get_it.dart';
 import 'package:neo_core/core/analytics/neo_logger.dart';
 
 abstract class _Constants {
-  static const Duration fetchTimeout = Duration(seconds: 60000);
-  static const Duration minimumFetchInterval = Duration.zero;
+  static const Duration fetchTimeout = Duration(minutes: 1);
+  static const Duration minimumFetchInterval = Duration(minutes: 15);
   static const String initializationFailMessage = "[NeoRemoteConfig]: FirebaseRemoteConfig fetchAndActivate failed!";
+  static const String setDefaultsFailMessage = "[NeoRemoteConfig]: FirebaseRemoteConfig setDefaults failed!";
 }
 
 class NeoRemoteConfig {
@@ -16,21 +17,29 @@ class NeoRemoteConfig {
   NeoLogger get _neoLogger => GetIt.I.get();
 
   Future<void> init() async {
-    await _firebaseRemoteConfig.setConfigSettings(
-      RemoteConfigSettings(
-        fetchTimeout: _Constants.fetchTimeout,
-        minimumFetchInterval: _Constants.minimumFetchInterval,
-      ),
-    );
+    try {
+      await _firebaseRemoteConfig.setConfigSettings(
+        RemoteConfigSettings(
+          fetchTimeout: _Constants.fetchTimeout,
+          minimumFetchInterval: _Constants.minimumFetchInterval,
+        ),
+      );
 
-    final bool isSuccess = await _firebaseRemoteConfig.fetchAndActivate();
-    if (!isSuccess) {
-      _neoLogger.logError(_Constants.initializationFailMessage);
+      final bool isSuccess = await _firebaseRemoteConfig.fetchAndActivate();
+      if (!isSuccess) {
+        _neoLogger.logError(_Constants.initializationFailMessage);
+      }
+    } catch (e) {
+      _neoLogger.logError("${_Constants.initializationFailMessage} Error: $e");
     }
   }
 
   Future<void> setDefaults({required Map<String, dynamic> defaultValues}) async {
-    await _firebaseRemoteConfig.setDefaults(defaultValues);
+    try {
+      await _firebaseRemoteConfig.setDefaults(defaultValues);
+    } catch (e) {
+      _neoLogger.logError("${_Constants.setDefaultsFailMessage} Error: $e");
+    }
   }
 
   RemoteConfigValue getValue({required String key}) => _firebaseRemoteConfig.getValue(key);
