fa64ce1c5f48de6833d359759b0dc7d15fec891b
U05434 <eboz@burgan.com.tr>
2024-11-05 17:55:01 +0300

    Dengage: listen click event for Dengage

--- lib/core/widgets/neo_core_messaging/neo_core_messaging.dart
+++ lib/core/widgets/neo_core_messaging/neo_core_messaging.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:neo_core/core/network/managers/neo_network_manager.dart';
 import 'package:neo_core/core/storage/neo_core_parameter_key.dart';
@@ -9,7 +10,7 @@ import 'package:neo_core/core/storage/neo_shared_prefs.dart';
 import 'package:neo_core/core/widgets/neo_core_firebase_messaging/neo_core_firebase_messaging.dart';
 import 'package:neo_core/core/widgets/neo_core_huawei_messaging/neo_core_huawei_messaging.dart';
 
-class NeoCoreMessaging extends StatelessWidget {
+class NeoCoreMessaging extends StatefulWidget {
   final Widget child;
   final NeoSharedPrefs neoSharedPrefs;
   final NeoNetworkManager networkManager;
@@ -30,31 +31,52 @@ class NeoCoreMessaging extends StatelessWidget {
   });
 
   @override
+  State<StatefulWidget> createState() => _NeoCoreMessagingState();
+}
+
+class _NeoCoreMessagingState extends State<NeoCoreMessaging> {
+  static const EventChannel eventChannel = EventChannel("com.dengage.flutter/onNotificationClicked");
+
+  void _onEvent(dynamic event) {
+    debugPrint("Dengage in on Event object is: $event");
+  }
+
+  void _onError(dynamic error) {
+    debugPrint("Dengage in on Error Object is: $error");
+  }
+
+  @override
   Widget build(BuildContext context) {
     if (kIsWeb) {
-      return child;
+      return widget.child;
     } else {
       final bool isHuaweiCompatible =
-          neoSharedPrefs.read(NeoCoreParameterKey.sharedPrefsIsHuaweiCompatible) as bool? ?? false;
+          widget.neoSharedPrefs.read(NeoCoreParameterKey.sharedPrefsIsHuaweiCompatible) as bool? ?? false;
       if (isHuaweiCompatible) {
         return NeoCoreHuaweiMessaging(
-          networkManager: networkManager,
-          neoCoreSecureStorage: neoCoreSecureStorage,
-          token: firebaseToken,
-          androidDefaultIcon: androidDefaultIcon,
-          onDeeplinkNavigation: onDeeplinkNavigation,
-          child: child,
+          networkManager: widget.networkManager,
+          neoCoreSecureStorage: widget.neoCoreSecureStorage,
+          token: widget.firebaseToken,
+          androidDefaultIcon: widget.androidDefaultIcon,
+          onDeeplinkNavigation: widget.onDeeplinkNavigation,
+          child: widget.child,
         );
       } else {
         return NeoCoreFirebaseMessaging(
-          networkManager: networkManager,
-          neoCoreSecureStorage: neoCoreSecureStorage,
-          token: firebaseToken,
-          androidDefaultIcon: androidDefaultIcon,
-          onDeeplinkNavigation: onDeeplinkNavigation,
-          child: child,
+          networkManager: widget.networkManager,
+          neoCoreSecureStorage: widget.neoCoreSecureStorage,
+          token: widget.firebaseToken,
+          androidDefaultIcon: widget.androidDefaultIcon,
+          onDeeplinkNavigation: widget.onDeeplinkNavigation,
+          child: widget.child,
         );
       }
     }
+  }
+
+  @override
+  void initState() {
+    eventChannel.receiveBroadcastStream().listen(_onEvent, onError: _onError);
+    super.initState();
   }
 }
