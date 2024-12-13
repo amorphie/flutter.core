e159d325343b882d0ec06ad81e1d1bb40aceef1a
U05434 <eboz@burgan.com.tr>
2024-12-13 19:30:43 +0300

    VCode: add print

--- lib/core/util/package_util.dart
+++ lib/core/util/package_util.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
 class PackageUtil {
@@ -7,6 +8,7 @@ class PackageUtil {
 
   Future<String> getAppVersionWithBuildNumber() async {
     _packageInfo ??= await PackageInfo.fromPlatform();
+    debugPrint("${_packageInfo?.version} - ${_packageInfo?.buildNumber}");
     return _appVersionWithBuildNumber ??= '${_packageInfo?.version}+${_packageInfo?.buildNumber}';
   }
 }
