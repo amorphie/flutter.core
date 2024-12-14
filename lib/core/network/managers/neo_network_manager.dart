3964436d5da5463eddda7ace3a255e0ff36f0ace
U05434 <eboz@burgan.com.tr>
2024-12-14 09:42:50 +0300

    VersionCode:  add function & logs

--- lib/core/network/managers/neo_network_manager.dart
+++ lib/core/network/managers/neo_network_manager.dart
@@ -13,6 +13,7 @@
 import 'dart:async';
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
@@ -96,6 +97,7 @@ class NeoNetworkManager {
   Future<void> init() async {
     await _initHttpClient();
     await _getTemporaryTokenForNotLoggedInUser(NeoHttpCall(endpoint: ""));
+    debugPrint("DELETE:${await PackageUtil().getAppVersionWithBuildNumber()}");
   }
 
   Future<Map<String, String>> get _defaultHeaders async {
--- lib/core/util/package_util.dart
+++ lib/core/util/package_util.dart
@@ -8,7 +8,17 @@ class PackageUtil {
 
   Future<String> getAppVersionWithBuildNumber() async {
     _packageInfo ??= await PackageInfo.fromPlatform();
-    debugPrint("${_packageInfo?.version} - ${_packageInfo?.buildNumber}");
+    debugPrint("DELETE: ${_packageInfo?.version} - ${_packageInfo?.buildNumber}");
+    debugPrint("DELETE: ${_packageInfo?.version}+${_packageInfo?.buildNumber}");
+    debugPrint("DELETE: $")
     return _appVersionWithBuildNumber ??= '${_packageInfo?.version}+${_packageInfo?.buildNumber}';
+  }
+
+  String convertToVersionName(int versionCode) {
+
+    final major = versionCode / 1000000 as int;
+    final minor = (versionCode % 1000000) / 10000 as int;
+    final patch = (versionCode % 10000) / 100 as int;
+    return "$major.$minor.$patch";
   }
 }
