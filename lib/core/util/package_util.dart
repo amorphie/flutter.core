c12bdbec55feeb80b007c996bdca033132b005af
U05434 <eboz@burgan.com.tr>
2024-12-14 10:02:32 +0300

    VersionCode: remove it

--- lib/core/util/package_util.dart
+++ lib/core/util/package_util.dart
@@ -10,12 +10,10 @@ class PackageUtil {
     _packageInfo ??= await PackageInfo.fromPlatform();
     debugPrint("DELETE: ${_packageInfo?.version} - ${_packageInfo?.buildNumber}");
     debugPrint("DELETE: ${_packageInfo?.version}+${_packageInfo?.buildNumber}");
-    debugPrint("DELETE: $")
     return _appVersionWithBuildNumber ??= '${_packageInfo?.version}+${_packageInfo?.buildNumber}';
   }
 
   String convertToVersionName(int versionCode) {
-
     final major = versionCode / 1000000 as int;
     final minor = (versionCode % 1000000) / 10000 as int;
     final patch = (versionCode % 10000) / 100 as int;
