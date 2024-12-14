7d96774689d5f82f68b9f2f92e8b4f661384ce93
U05434 <eboz@burgan.com.tr>
2024-12-14 10:31:10 +0300

    VersionCode: add functions for calculation

--- lib/core/util/package_util.dart
+++ lib/core/util/package_util.dart
@@ -1,22 +1,38 @@
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
+import 'package:get_it/get_it.dart';
 import 'package:package_info_plus/package_info_plus.dart';
+
+import '../analytics/neo_logger.dart';
 
 class PackageUtil {
   static PackageInfo? _packageInfo;
 
   static String? _appVersionWithBuildNumber;
+  late final NeoLogger _neoLogger = GetIt.I.get();
+  static const _errMessage = "[PackageUtil]: versionCode cannot be null";
 
   Future<String> getAppVersionWithBuildNumber() async {
     _packageInfo ??= await PackageInfo.fromPlatform();
     debugPrint("DELETE: ${_packageInfo?.version} - ${_packageInfo?.buildNumber}");
     debugPrint("DELETE: ${_packageInfo?.version}+${_packageInfo?.buildNumber}");
-    return _appVersionWithBuildNumber ??= '${_packageInfo?.version}+${_packageInfo?.buildNumber}';
+    if (Platform.isAndroid) {
+      return _getBuildNoFromFormattedVersionCode(_packageInfo?.buildNumber);
+    } else {
+      return _appVersionWithBuildNumber ??= '${_packageInfo?.version}+${_packageInfo?.buildNumber}';
+    }
   }
 
-  String convertToVersionName(int versionCode) {
-    final major = versionCode / 1000000 as int;
-    final minor = (versionCode % 1000000) / 10000 as int;
-    final patch = (versionCode % 10000) / 100 as int;
-    return "$major.$minor.$patch";
+  String _getBuildNoFromFormattedVersionCode(String? versionCode) {
+    if (versionCode == null) {
+      _neoLogger.logError(_errMessage);
+      throw ArgumentError(_errMessage);
+    }
+    final vCode = versionCode as int;
+    final major = vCode / 1000000 as int;
+    final minor = (vCode % 1000000) / 10000 as int;
+    final patch = (vCode % 10000) / 100 as int;
+    return (vCode - (major + minor + patch)).toString();
   }
 }
