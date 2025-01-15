c0a60d612c636e7e633e0dbba62e41118f67e0cf
Emre Reyhanlıoğlu <reyhanliogluemre@gmail.com>
2025-01-15 10:57:55 +0300

    Develop: Await onLoggedInSuccessfully method

--- lib/core/widgets/neo_transition_listener/neo_transition_listener_widget.dart
+++ lib/core/widgets/neo_transition_listener/neo_transition_listener_widget.dart
@@ -14,7 +14,7 @@ class NeoTransitionListenerWidget extends StatelessWidget {
   final String signalRMethodName;
   final Function(SignalrTransitionData navigationData) onTransitionEvent;
   final Function(EkycEventData eventData) onEkycEvent;
-  final Function({required bool isTwoFactorAuthenticated})? onLoggedInSuccessfully;
+  final Future Function({required bool isTwoFactorAuthenticated})? onLoggedInSuccessfully;
   final Function(NeoError error, {required bool displayAsPopup})? onError;
   final Function({required bool displayLoading}) onLoadingStatusChanged;
   final Duration signalrLongPollingPeriod;
