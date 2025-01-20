d060ad43bbdd9f1d6bbcca2296bebc5f82af20f6
Alperen Soysal <alperen.soysal@commencis.com>
2025-01-20 17:25:42 +0300

    AdjustDeferredLink: Refactor code

--- lib/core/analytics/neo_adjust.dart
+++ lib/core/analytics/neo_adjust.dart
@@ -6,6 +6,7 @@ import 'package:neo_core/neo_core.dart';
 
 class NeoAdjust {
   final void Function(String?) adjustDeferredDeeplinkCallback;
+
   NeoAdjust({required this.adjustDeferredDeeplinkCallback});
 
   Future<void> init({required String appToken}) async {
