551c83d64a93a1477094856381de2103f581741b
U05434 <eboz@burgan.com.tr>
2025-01-30 10:40:54 +0300

    Huawei: update scheme for the payload of the messaging-gateway

--- lib/feature/neo_push_message_payload_handlers/neo_push_message_payload_handler.dart
+++ lib/feature/neo_push_message_payload_handlers/neo_push_message_payload_handler.dart
@@ -1,5 +1,6 @@
 abstract class NeoPushMessagePayloadHandler {
   static const pushNotificationDeeplinkKey = "deeplink";
+  static const pushNotificationHuaweiDeeplinkExtrasKey = "extras";
 
   void handleMessage({required dynamic message, required Function(String)? onDeeplinkNavigation});
 }
