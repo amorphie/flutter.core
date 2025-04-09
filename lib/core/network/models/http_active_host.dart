469faa226778d987f6bd71760f0fd42f64a1d193
Emre Reyhanlıoğlu <reyhanliogluemre@gmail.com>
2025-04-09 16:40:01 +0300

    Develop: Refactor code for debug menu feature

--- lib/core/network/models/http_active_host.dart
+++ lib/core/network/models/http_active_host.dart
@@ -10,19 +10,42 @@
  * Any reproduction of this material must contain this notice.
  */
 
+import 'dart:convert';
+
+import 'package:equatable/equatable.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'http_active_host.g.dart';
 
+abstract class _Constants {
+  static const keyHost = 'host';
+  static const keyRetryCount = 'retry-count';
+}
+
 @JsonSerializable(createToJson: false)
-class HttpActiveHost {
-  @JsonKey(name: 'host', defaultValue: "")
+class HttpActiveHost extends Equatable {
+  @JsonKey(name: _Constants.keyHost, defaultValue: "")
   final String host;
 
-  @JsonKey(name: 'retry-count', defaultValue: 0)
+  @JsonKey(name: _Constants.keyRetryCount, defaultValue: 0)
   final int retryCount;
 
   const HttpActiveHost({required this.host, required this.retryCount});
 
   factory HttpActiveHost.fromJson(Map<String, dynamic> json) => _$HttpActiveHostFromJson(json);
+
+  String encode() {
+    return jsonEncode({
+      _Constants.keyHost: host,
+      _Constants.keyRetryCount: retryCount,
+    });
+  }
+
+  factory HttpActiveHost.decode(String jsonString) {
+    final Map<String, dynamic> json = jsonDecode(jsonString);
+    return HttpActiveHost.fromJson(json);
+  }
+
+  @override
+  List<Object?> get props => [host, retryCount];
 }
