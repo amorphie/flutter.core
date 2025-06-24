4c46b6316f33d3933b60f90304ec002815d55619
U05434 <eboz@burgam.com.tr>
2025-06-24 13:49:37 +0300

    dev: rearrange import

--- lib/core/widgets/neo_core_huawei_messaging/neo_core_huawei_messaging.dart
+++ lib/core/widgets/neo_core_huawei_messaging/neo_core_huawei_messaging.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
+
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
