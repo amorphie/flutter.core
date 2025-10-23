efe976ca1cc07e3777ee3497aabdd209024e3c9f
Emre Reyhanlioglu <reyhanliogluemre@gmail.com>
2025-10-23 14:00:34 +0300

    Delete wrong copyrights

--- lib/core/localization/bloc/localization_state.dart
+++ lib/core/localization/bloc/localization_state.dart
@@ -1,15 +1,3 @@
-/*
- * neo_bank
- *
- * Created on 4/10/2023.
- * Copyright (c) 2023 BurganBank. All rights reserved.
- *
- * Save to the extent permitted by law, you may not use, copy, modify,
- * distribute or create derivative works of this material or any part
- * of it without the prior written consent of BurganBank.
- * Any reproduction of this material must contain this notice.
- */
-
 part of 'localization_bloc.dart';
 
 class LocalizationState extends Equatable {
